a0a2f30a2e0394dbbaceb1c75196ac41d69ab71c
Vaibhavi Bandaru <vaibhavi.bandaru7@gmail.com>
2025-07-15 11:46:38 -0400

    Made base layout for home page in contextView file

--- cloudrestApp/ContentView.swift
+++ cloudrestApp/ContentView.swift
@@ -9,13 +9,84 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(alignment: .leading, spacing: 20) {
+            Text(Date(), style: .date)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+            Text("here is a recap of your month so far")
+                .foregroundColor(.yellow)
+                .font(.subheadline)
+
+            
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 150.0)
+                    Text("number")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                Text("your average amount of sleep")
+                    .padding(.leading, 15)
+            }
+            
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 150.0)
+                    Text("number")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                Text("your longest rest so far")
+                    .padding(.leading, 15)
+            }
+            
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 150.0)
+                    Text("number")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                Text("your average sleep quality")
+                    .padding(.leading, 15)
+            }
+            
+            Text("activities that improve your sleep")
+            Button("listening to music") {
+                
+            }
+            .font(.title2)
+            .buttonStyle(BorderedProminentButtonStyle())
+            .tint(Color(hue: 0.702, saturation: 0.516, brightness: 0.953))
+            
+            Button("exercising") {
+                
+            }
+            .font(.title2)
+            .buttonStyle(BorderedProminentButtonStyle())
+            .tint(Color(hue: 0.702, saturation: 0.516, brightness: 0.953))
+            
+            Text("habit to improve on")
+            Button("screentime before bed") {
+                
+            }
+            .font(.title2)
+            .buttonStyle(BorderedProminentButtonStyle())
+            .tint(Color(hue: 0.702, saturation: 0.516, brightness: 0.953))
+            
+            Spacer()
         }
         .padding()
+        
+        .toolbar {
+            
+        }
+        
     }
 }
 
