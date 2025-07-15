b520d2a6a42489ca55a162b29cb1f478e87802b6
Anna Wang <annawang378@gmail.com>
2025-07-15 17:34:27 -0400

    added about page, fixed spacing on log

---
+++ cloudrestApp/about.swift
@@ -0,0 +1,18 @@
+//
+//  about.swift
+//  cloudrestApp
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+struct about: View {
+    var body: some View {
+        Text("about")
+    }
+}
+
+#Preview {
+    about()
+}
