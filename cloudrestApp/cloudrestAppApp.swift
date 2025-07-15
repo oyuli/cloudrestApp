8e560d856a199dc90c1c3fc05d9b0b5c3107f7ee
Anna Wang <annawang378@gmail.com>
2025-07-15 10:58:48 -0400

    Initial Commit

---
+++ cloudrestApp/cloudrestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudrestAppApp.swift
+//  cloudrestApp
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct cloudrestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
