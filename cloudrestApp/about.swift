5bd2e7389dc1fe8c767a83d76f5b4a72084c8d0c
Anna Wang <annawang378@gmail.com>
2025-07-16 13:43:18 -0400

    fixed errors in about

--- cloudrestApp/about.swift
+++ cloudrestApp/about.swift
@@ -7,44 +7,39 @@
 
 import SwiftUI
 
+
 struct about: View {
     var body: some View {
-        Text("about")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("cloudrest - Sleep Tracker")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color(red: 0.851, green: 0.765, blue: 0.416))
+                Text("Welcome to Cloudiest, your companion for restful nights and mindful sleep tracking.")
+                Group {
+                    Text("Our Mission")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.purple)
+                    Text("We help you understand and improve your sleep habits with a simple, calm design.")
+                    Text("Design vision")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.purple)
+                    Text("Sweet dreams")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(.yellow)
+                }
+                .padding()
+                .background(Color.black)
+                .foregroundColor(.white)
+            }
+            .padding()
+        }
     }
 }
-
 #Preview {
     about()
-}
-ScrollView {
-    VStack (alignment: .leading, spacing: 20) }
-Text("Cloudiest - Sleep Traker")
-    .font(.largeTitle
-        .fontWeight(.bold)
-        .foregroundColor (.yellow)
-          Text("Welcome to Cloudiest, your companion for restful nights and mindful sleep tracking.")
-        .font(.body)
-         
-          Group {
-        Text ("Our Mission")
-            .font(.title2)
-            .fontWeight(.semibold)
-            .foregroundColor(.purple)
-        
-        Text("We help you understand and impove your sleep habits with a simple, clam and design")
-        Text("Design vision")
-            .font(.title2)
-            .fontWeigght(.semibold)
-            .foregroundColor(.purple)
-        
-        Text("sweet dreams")
-            .font(.title3)
-            .fontWeight(.medium)
-            .foregroundColor(.yellow)
-    }
-        .padding()
-        .background(Color.black)
-        .foregroundColor(Color(UIColor)
-         }
-     }
 }
