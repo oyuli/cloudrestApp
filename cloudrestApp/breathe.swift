120ef41c3d510d1e8949683ff0da141abb16ccdc
Vaibhavi Bandaru <vaibhavi.bandaru7@gmail.com>
2025-07-16 10:49:25 -0400

    Image animation in breathe file

--- cloudrestApp/breathe.swift
+++ cloudrestApp/breathe.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct breathe: View {
+    
+    @State private var imageOffset: CGFloat = 0 //image initial position
+    @State private var timer: DispatchSourceTimer? //timer for animation
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("SheepClosed")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.top, .leading, .trailing], 30)
+                    .padding(.bottom, 5.0)
+                    .offset(y: imageOffset)
+                
+                Button("Start") {
+                    startAnimation()
+                }
+                .foregroundColor(Color("Headings"))
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.bottom, 30.0)
+                .kerning(2)
+            }
+        }
     }
+    
+    func startAnimation() {
+        
+        let queue = DispatchQueue(label: "com.example.timerQueue")
+        let timer = DispatchSource.makeTimerSource(queue: queue)
+        
+        //move image up
+        withAnimation(.easeInOut(duration: 3)) {
+            imageOffset = -400
+        }
+        
+        //timer set up
+        timer.setEventHandler {
+            //image hold 2 seconds
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation(.easeInOut(duration: 4)) {
+                    imageOffset = 0
+                }
+            }
+            timer.cancel()
+        }
+        
+        //starts when image is up
+        timer.schedule(deadline: .now() + 3)
+        
+        //activating timer
+        self.timer = timer
+        timer.activate()
+        
+    }
+    
+    
+    
 }
 
 #Preview {
